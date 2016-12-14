92699a312f7377bb256770a80bfcbf6fa52bcb9f
Timothy Hennessy <timothy.hennessy23@gmail.com>
2016-12-14 10:58:10 -0500

    added code

--- sem.h
+++ sem.h
@@ -1,1 +1,16 @@
+#define FALSE 0
+#define TRUE  1
+#define N 3 // num of shared resources controlled by semaphore
+#define NUM_THREADS 5
 
+typedef struct
+{
+    int turns;
+    int bWaiting;
+} Element;
+
+typedef struct
+{
+    int n;
+    Element *waitlist;
+} Semaphore;
